6d3c4f5f798dc929fffd3a36dc3179140230da2c
sethbientus <sethbientus@gmail.com>
2019-06-09 22:56:10 +0200

    removed

--- project1/serverSide/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    if(isset($_GET['signout'])){
-        session_destroy();
-        header("location: ../pages/Home.php");
-    }
-?>
--- wordpress/wp-content/plugins/customized-plugins/serverSide/insert_about_us.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require '../Database_Handler/ActivitiesHandler.php';
-	$activity = new ActivitiesHandler();
-	if (isset($_POST["submit"])) {
-		$mission = $_POST["mission"];
-		$information = $_POST["information"];
-		$activity->insert_about($mission,$information);
-	}
-?>
